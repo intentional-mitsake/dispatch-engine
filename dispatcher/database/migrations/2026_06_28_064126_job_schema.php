3ea768ee815f83ec3a8f69bf23decb93b1dd0ef7
gaurav munankarmi <96043792+gaurab7@users.noreply.github.com>
2026-06-28 13:08:08 +0545

    jobs->dispatches

--- dispatcher/database/migrations/2026_06_28_064126_job_schema.php
+++ dispatcher/database/migrations/2026_06_28_064126_job_schema.php
@@ -12,19 +12,22 @@ return new class extends Migration
     public function up(): void
     {
         // this will create the jobs table
-        Schema::create('jobs', function(Blueprint $table) {
+        Schema::create('dispatches', function(Blueprint $table) {
             $table->id();// primary key
+
             $table->string('type'); // job type--> payment or inventory
             $table->jsonb('payload'); // job payload(data) --> pyement(amount & others) or inventory(product_id, quantity)
             $table->string('status')->default('pending');
-            $table->unsingnedInteger('attempts')->default(0); // number of attempts
+            $table->unsignedInteger('attempts')->default(0); // number of attempts
             $table->string('idempotency_key')->unidque(); // thsi just make sure that a job is not processed twice
             $table->string('claimed_by')->nullable(); // which worker claimed the jib
-            $table->timestamps('claimed_at')->nullable();
-            $table->timestamps('available_at')->useCurrent(); // when the job is available-->current time
-            $table->timestamps('failed_at')->nullable();
+
+            $table->timestamp('claimed_at')->nullable();
+            $table->timestamp('available_at')->useCurrent(); // when the job is available-->current time
+            $table->timestamp('failed_at')->nullable();
+
             $table->timestamps(); // in laravel this auto adds created_at and updated_at
-        })
+        });
     }
 
     /**
@@ -35,6 +38,6 @@ return new class extends Migration
         // down is exact reverse of up
         // up runs when migration is applied, down runs when migration is reversed
         // so if create in up, delete in down
-        Schema::dropIfExists('jobs');
+        Schema::dropIfExists('dispatches');
     }
 };

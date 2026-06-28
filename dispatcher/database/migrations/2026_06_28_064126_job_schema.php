b444b70e55d285069987684e2a47d74ce2fd66c1
gaurav munankarmi <96043792+gaurab7@users.noreply.github.com>
2026-06-28 12:49:44 +0545

    init job shcema

--- dispatcher/database/migrations/2026_06_28_064126_job_schema.php
+++ dispatcher/database/migrations/2026_06_28_064126_job_schema.php
@@ -11,7 +11,20 @@ return new class extends Migration
      */
     public function up(): void
     {
-        //
+        // this will create the jobs table
+        Schema::create('jobs', function(Blueprint $table) {
+            $table->id();// primary key
+            $table->string('type'); // job type--> payment or inventory
+            $table->jsonb('payload'); // job payload(data) --> pyement(amount & others) or inventory(product_id, quantity)
+            $table->string('status')->default('pending');
+            $table->unsingnedInteger('attempts')->default(0); // number of attempts
+            $table->string('idempotency_key')->unidque(); // thsi just make sure that a job is not processed twice
+            $table->string('claimed_by')->nullable(); // which worker claimed the jib
+            $table->timestamps('claimed_at')->nullable();
+            $table->timestamps('available_at')->useCurrent(); // when the job is available-->current time
+            $table->timestamps('failed_at')->nullable();
+            $table->timestamps(); // in laravel this auto adds created_at and updated_at
+        })
     }
 
     /**
@@ -19,6 +32,9 @@ return new class extends Migration
      */
     public function down(): void
     {
-        //
+        // down is exact reverse of up
+        // up runs when migration is applied, down runs when migration is reversed
+        // so if create in up, delete in down
+        Schema::dropIfExists('jobs');
     }
 };

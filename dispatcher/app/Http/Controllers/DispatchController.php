e6605d376bba726bb71be131a890fcce9d35acdc
gaurav munankarmi <96043792+gaurab7@users.noreply.github.com>
2026-06-28 13:44:49 +0545

    added idempotency check

--- dispatcher/app/Http/Controllers/DispatchController.php
+++ dispatcher/app/Http/Controllers/DispatchController.php
@@ -11,12 +11,22 @@ class DispatchController extends Controller
         $validated = $request->validate([ // validate incoming request-->for things like sql injection
             'type' => 'required|string',
             'payload' => 'required|array',
+            'idempotency_key' => 'required|string',
         ]);
+
+        // basic sql to find the first row in the table with the same idempotency key
+        $existingDispatch = Dispatch::where('idempotency_key', $validated['idempotency_key'])->first();
+        if ($existingDispatch) {
+            return response()->json(['message' => 'Duplicate request'], 200); // OK, move on
+        }
 
         $dispatch = Dispatch::create([
             'type' => $validated['type'],
             'payload' => $validated['payload'],
-            'idempotency_key' => Str::uuid(), // generate unique id
+            // if we gen new idempotency key for each request at server it makes each request unique
+            // this means if user clicks on the button multiple times it will register as multiple requests
+            // so we can create idempotency key at client side and check if it is unique to prevent duplicate requests
+            'idempotency_key' => $validated['idempotency_key'],
         ]);
         return response()->json(['message' => 'Dispatch created successfully', 'data' => $dispatch], 201);
     }

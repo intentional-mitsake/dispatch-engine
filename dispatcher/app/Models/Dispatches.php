9ed2d72603d89eb2a8bf86c0b4807a61b9325f29
gaurav munankarmi <96043792+gaurab7@users.noreply.github.com>
2026-06-28 13:20:20 +0545

    Model definition

--- dispatcher/app/Models/Dispatches.php
+++ dispatcher/app/Models/Dispatches.php
@@ -4,7 +4,33 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Dispatches extends Model
+class Dispatch extends Model
 {
-    //
+    // PHP auto recognize stuff like fillable, casts...
+    protected $table = 'dispatches'; // this is the table name
+    protected $fillable = [ // defines fields that can be mass assigned or be assigned by the fill method(like create method)
+        'type',
+        'payload',
+        'status',
+        'attempts',
+        'idempotency_key',
+        'claimed_by',
+        'claimed_at',
+        'available_at',
+        'failed_at'
+    ];
+
+    protected $casts = [ // converts Postgres data types to PHP data types
+        'payload' => 'array',
+        'attempts' => 'integer',
+        'claimed_at' => 'datetime',
+        'available_at' => 'datetime',
+        'failed_at' => 'datetime'
+        // the other 4 fields wre string, auto string in PHP also, only needed to convert these
+    ];
+
+    protected $attributes = [ // default values
+        'status' => 'pending',
+        'attempts' => 0,
+    ];
 }

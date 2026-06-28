38444fd79ce98d54f8f364094fc96c888793462f
gaurav munankarmi <96043792+gaurab7@users.noreply.github.com>
2026-06-28 13:48:15 +0545

    wired routes

--- dispatcher/routes/web.php
+++ dispatcher/routes/web.php
@@ -5,3 +5,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::post('/dispatch', [App\Http\Controllers\DispatchController::class, 'store']);
+Route::get('/dispatch', [App\Http\Controllers\DispatchController::class, 'show']);

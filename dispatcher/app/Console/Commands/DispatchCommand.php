f14ae9ff0ccd1746951ac9b203a3f5be51697482
gaurav munankarmi <96043792+gaurab7@users.noreply.github.com>
2026-06-29 12:40:53 +0545

    initial processing

--- dispatcher/app/Console/Commands/DispatchCommand.php
+++ dispatcher/app/Console/Commands/DispatchCommand.php
@@ -5,6 +5,7 @@ namespace App\Console\Commands;
 use Illuminate\Console\Attributes\Description;
 use Illuminate\Console\Attributes\Signature;
 use Illuminate\Console\Command;
+use App\Models\Dispatch;
 
 #[Signature('app:dispatch-command')]
 #[Description('Command description')]
@@ -15,6 +16,21 @@ class DispatchCommand extends Command
      */
     public function handle()
     {
-        //
+        while(true) {
+            $dispatch = Dispatch::where('status', 'pending')->where('available_at', '<=', now())->first();
+            if ($dispatch) {
+                $this->process($dispatch);
+            } else {
+                sleep(4); // if no pending jobs, sleep for 4 seconds
+            }
+        }
+    }
+
+    private function process(Dispatch $dispatch) {
+        $dispatch->update([
+            'status' => 'processing'
+            'claimed_at' => now(),
+            'claimed_by' => gethostname(),// get current hostname-->.i.e localhost
+        ])
     }
 }

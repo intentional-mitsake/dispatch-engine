416e726cdf42517f26472dd1b7cf2afbf39e0d7e
gaurav munankarmi <96043792+gaurab7@users.noreply.github.com>
2026-06-29 14:04:29 +0545

    seeder logic

--- dispatcher/database/seeders/DispatchSeeder.php
+++ dispatcher/database/seeders/DispatchSeeder.php
@@ -5,14 +5,21 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Log;
+use App\Models\Dispatch;
 
 class DispatchSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
-        //
+        foreach (range(1, 20) as $i) {
+            Dispatch::create([
+                'type' => 'payment',
+                'idempotency_key' => 'pay-ord-' . $i,
+                'payload' => [ 
+                    'amount' => rand(1, 100),
+                    'customer_id' => 'user-'. $i . 'test',
+                ]
+            ]);
+        }
     }
 }

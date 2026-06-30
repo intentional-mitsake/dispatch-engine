10fbd443bf583fe0dfb3ea87fce0ffbe39be3c41
gaurav munankarmi <96043792+gaurab7@users.noreply.github.com>
2026-06-30 12:09:42 +0545

    force fail

--- dispatcher/app/Handlers/PaymentHandler.php
+++ dispatcher/app/Handlers/PaymentHandler.php
@@ -7,7 +7,11 @@ use Illuminate\Support\Facades\Log;
 
 class PaymentHandler
 {
-    public function handle(Dispatch $dispatch) {
+    public function handle(Dispatch $dispatch, bool $failure) {
+        if ($failure) {
+            Log::error("Payment failed for dispatch {$dispatch->payload['customer_id']}");
+            throw new \Exception('Payment failed');
+        }
         // this function purely simulates payment process so that we can try out diff cases
         sleep(rand(1, 10));// sleep for random amount of time from 1 to 10 seconds
 

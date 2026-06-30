70e6920138d30e44be46c1e4fae3dafe6a840491
gaurav munankarmi <96043792+gaurab7@users.noreply.github.com>
2026-06-30 12:03:29 +0545

    failure handler func

---
+++ dispatcher/app/Handlers/FailureHandlers.php
@@ -0,0 +1,34 @@
+<?php
+
+use App\Models\Dispatch;
+
+class FailureHandler {
+    private const MAX_ATTEMPTS = 5;
+    // ref from HackerOne
+    public function handle(Dispatch $dispatch, \Throwable $exception) {
+        $dispatch->increment('attempts');
+        $dispatch->refresh(); // reloads the model from updated values in database
+        // only change status to failed if the max attempts has been reached
+        if($dispatch->attempts >= self::MAX_ATTEMPTS) {
+            $dispatch->update([
+                'status' => 'failed',
+                'failed_at' => now(),
+            ]);
+            return;
+        }
+        // get the delay for the next retry
+        $delay = $this->backoffWithJitter($dispatch->attempts);
+        // update the status to pending, and set the available_at to now + delay 
+        // so that it can only be claimed after the delay
+        $dispatch->update([
+            'status' => 'pending',
+            'available_at' => now()->addSeconds($delay),
+        ]);
+    }
+
+    private function backoffWithJitter(int $attempt): int {
+        $baseDelay = 2 ** $attempt; // exponential backoff--> 2^1, 2^2, 2^3
+        $jitter = rand(0, 3; // jitter to avoid synchronization of retries
+        return $baseDelay + $jitter;
+    }
+}
